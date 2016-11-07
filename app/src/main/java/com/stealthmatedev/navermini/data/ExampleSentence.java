26ba91a5a30d98bf2fbebae6c09ba966aa9e1163
Stealthmate <stealthmate1233@gmail.com>
2016-11-07 17:48:42 +0200

    Updated gitignore

---
+++ app/src/main/java/com/stealthmatedev/navermini/data/ExampleSentence.java
@@ -0,0 +1,10 @@
+package com.stealthmatedev.navermini.data;
+
+/**
+ * Created by Stealthmate on 16/11/07 0007.
+ */
+
+public class ExampleSentence {
+    public final SentenceEntry.Language from;
+    public final SentenceEntry.Language to;
+}
